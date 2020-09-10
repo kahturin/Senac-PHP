77cda03b8dfaea69f6a7b44d80393fd2d347bbb1
Karina alves <karinaalveslima@icloud.com>
2020-09-10 19:03:09 -0300

    repeticoes com vetores

--- repeticoes/index.php
+++ repeticoes/index.php
@@ -1,25 +1,70 @@
 <?php
 
+date_default_timezone_set('America/Sao_Paulo');
+
 for ($i = 0, $j = 4 ; $i < 5 ; $i++, $j--) {
-
-    echo "Linha: $i $j<br>";
+    echo "linha: $i $j<br>";
 }
-
-#exemplo de while
+echo "<br><br>";
+// exemplo de while
 $i = 0;
-
-while ($i < 5){
-
-    echo "Linha: $i <br>";
+$j = 4;
+while ($i < 5) {
+    echo "linha: $i $j<br>";
 
     $i++;
-
+    $j--;
 }
- 
-$i = 0;
 
+echo "<br><br>";
+// exemplo do while
+$i = 0;
+$j = 4;
 do {
-    echo "Linha: $i <br>";
+    echo "Linha: $i $j<br>";
 
     $i++;
+    $j--;
+} while ($i < 5);
+
+$domingo = 0;
+$segunda = 1;
+$terca = 2;
+$quarta = 3;
+$quinta = 4;
+$sexta = 5;
+$sabado = 6;
+
+var_dump(date('d/m/Y'));
+
+switch (date('w')) {
+    case 'domingo';
+        echo 'Domingo';
+break;
+    case 'segunda':
+        echo 'Segunda';
+break;
+    case 'terca':
+         echo 'Terca';
+break;
+    case 'quarta':
+        echo 'Quarta';
+break;
+    case 'quinta':
+        echo 'Quinta';
+break;
+    case 'sexta':
+        echo 'Sexta';
+break;
+    case 'sabado':
+        echo 'Sabado';
+break;
 }
+
+$semana[0] = 'Domingo';
+$semana[1] = 'Segunda';
+$semana[2] = 'Terca';
+$semana[3] = 'Quarta';
+$semana[4] = 'Quinta';
+$semana[5] = 'Sexta';
+$semana[6] = 'Sabado';
